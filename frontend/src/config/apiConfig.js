88be6854ce89bf9c829312f7f15bda48dcce862a
imimran <engr.aih@gmail.com>
2022-01-06 19:26:10 +0600

    Add Employee form

---
+++ frontend/src/config/apiConfig.js
@@ -0,0 +1,7 @@
+const apiBaseUrl = "http://localhost:5000";
+const baseUrl = "http://localhost:3000";
+
+module.exports = {
+    apiBaseUrl: apiBaseUrl,
+    baseUrl: baseUrl,
+}
---
+++ frontend/src/screens/AddEmployeeScreen.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import { Form, Button } from "react-bootstrap";
+import { apiBaseUrl } from "../config/apiConfig";
+import { useForm } from "react-hook-form";
+
+function AddEmployeeScreen() {
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleEmployeeSubmit = (values) => {
+    console.log("values", values);
+
+    const userData = new FormData();
+    userData.append("first_name", values.first_name ? values.first_name : "");
+    userData.append("last_name", values.last_name ? values.last_name : "");
+    userData.append("email", values.email ? values.email : "");
+
+    Axios.post(apiBaseUrl + "/api/v1/employee/create", userData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <Form
+        className="card card-body p-5 m-5 col-lg-6 mx-auto"
+        onSubmit={handleSubmit(handleEmployeeSubmit)}
+      >
+        <h5> Add New Employee </h5>
+        <Form.Group controlId="message">
+          <Form.Label> First Name:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter First Name"
+            defaultValue={first_name}
+            onChange={(e) => setFirstName(e.target.value)}
+            className={errors.first_name && "is-invalid"}
+            {...register("first_name", {
+              required: true,
+              minLength: 3,
+              maxLength: 20,
+            })}
+          />
+          {errors.first_name && (
+            <span className="text-danger">
+              {errors.first_name.type === "required" &&
+                "Please give first_name"}
+              {errors.first_name.type === "minLength" &&
+                "Please give first_name minimum of 5 characters"}
+              {errors.first_name.type === "maxLength" &&
+                "Please give first_name maximum of 50 characters"}
+            </span>
+          )}
+        </Form.Group>
+
+        <Form.Group controlId="message">
+          <Form.Label> Last Name:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Last Name"
+            defaultValue={last_name}
+            onChange={(e) => setLastName(e.target.value)}
+            className={errors.last_name && "is-invalid"}
+            {...register("last_name", {
+              required: true,
+              minLength: 3,
+              maxLength: 20,
+            })}
+          />
+          {errors.last_name && (
+            <span className="text-danger">
+              {errors.last_name.type === "required" && "Please give last_name"}
+              {errors.last_name.type === "minLength" &&
+                "Please give last_name minimum of 3 characters"}
+              {errors.last_name.type === "maxLength" &&
+                "Please give last_name maximum of 50 characters"}
+            </span>
+          )}
+        </Form.Group>
+
+        <Form.Group controlId="email">
+          <Form.Label>Email Address:</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter Email Address"
+            defaultValue={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className={errors.email && "is-invalid"}
+            {...register("email", {
+              required: "required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Enter a valid e-mail address",
+              },
+            })}
+          />
+          {errors.email && (
+            <span className="text-danger">
+              {errors.email.type === "required" && "Please give email address"}
+              {errors.email.type === "pattern" &&
+                "Please give a valid email address"}
+            </span>
+          )}
+        </Form.Group>
+
+        <br />
+        <Button type="submit" variant="primary">
+          Create Employee
+        </Button>
+      </Form>
+    </>
+  );
+}
+
+export default AddEmployeeScreen;
