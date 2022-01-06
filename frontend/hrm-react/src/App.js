0c0496772f3f22af3d84047f0dd305c83c571cde
imimran <engr.aih@gmail.com>
2022-01-06 17:37:59 +0600

    Create-React-App

---
+++ frontend/hrm-react/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import AddEmployeeScreen from './screens/AddEmployeeScreen';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AddEmployeeScreen/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/hrm-react/src/screens/AddEmployeeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddEmployeeScreen() {
+    return (
+        <div className='container mt-5'>
+            <button className='btn btn-primary'>Submit</button>
+        </div>
+    )
+}
+
+export default AddEmployeeScreen
